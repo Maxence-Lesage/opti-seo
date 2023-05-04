29c31d4b5f23d889c4667fd6442b3555a05e2614
Maxence Lesage <maxencelesage4@gmail.com>
2023-05-04 13:53:28 +0200

    Test

---
+++ purgecss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    content: ['./index.html', './assets/maugallery.js', './assets/scripts.js', './assets/bootstrap/bootstrap.bundle.min.js'],
+    css: ['./assets/bootstrap/bootstrap.min.css'],
+    safelist: ['mb-4', 'col-12', 'col-sm-6', 'col-md-4', 'col-lg-4', 'col-xl-4']
+}
